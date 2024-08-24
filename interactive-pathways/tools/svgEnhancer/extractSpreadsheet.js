e1913a4ac9d9b92a5116b274251e7ecb47d43693
Irvin Szeto <irvins@stanford.edu>
2024-08-23 22:14:04 -0700

    the connection to the google sheets is verified now via the service account for the GCP project , so it generates one of the required json files at least

--- interactive-pathways/tools/svgEnhancer/extractSpreadsheet.js
+++ interactive-pathways/tools/svgEnhancer/extractSpreadsheet.js
@@ -1,3 +1,9 @@
+const { google } = require('googleapis');
+const fs = require('fs');
+const R = require('ramda');
+const S = require('underscore.string.fp');
+const { createGreekEquivalentNames, replaceGreekPhoneticWithGreekLetters } = require('./utils/greek');
+
 const config = {
     spreadSheetId: '1k8xIVzpx5aV839SHc-FzGTSHTDyLhYl8yVqyCjPj5ck',
     spreadSheetTabName: 'Unique Text Dump',
@@ -5,60 +11,19 @@ const config = {
     typeColumnOffset: 3,
 };
 
-const {
-    createGreekEquivalentNames,
-    replaceGreekPhoneticWithGreekLetters,
-} = require('./utils/greek');
-
-const fs = require('fs');
-const readline = require('readline');
-const { google } = require('googleapis');
-
-const R = require('ramda');
-const S = require('underscore.string.fp');
-
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
-const TOKEN_PATH = 'token.json';
-
-fs.readFile('credentials.json', (err, content) => {
-    if (err) return console.log('Error loading client secret file:', err);
-    authorize(JSON.parse(content), getSubstancesAndTypes);
-});
-
-function authorize(credentials, callback) {
-    const { client_secret, client_id, redirect_uris } = credentials.installed;
-    const oAuth2Client = new google.auth.OAuth2(
-        client_id, client_secret, redirect_uris[0]);
-
-    fs.readFile(TOKEN_PATH, (err, token) => {
-        if (err) return getNewToken(oAuth2Client, callback);
-        oAuth2Client.setCredentials(JSON.parse(token));
-        callback(oAuth2Client);
+async function authenticate() {
+    const auth = new google.auth.JWT({
+        keyFile: './credentials.json', // Path to your service account key file
+        scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
     });
-}
 
-function getNewToken(oAuth2Client, callback) {
-    const authUrl = oAuth2Client.generateAuthUrl({
-        access_type: 'offline',
-        scope: SCOPES,
-    });
-    console.log('Authorize this app by visiting this URL:', authUrl);
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
-    rl.question('Enter the code from that page here: ', (code) => {
-        rl.close();
-        oAuth2Client.getToken(code, (err, token) => {
-            if (err) return console.error('Error while trying to retrieve access token', err);
-            oAuth2Client.setCredentials(token);
-            fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-                if (err) console.error(err);
-                console.log('Token stored to', TOKEN_PATH);
-            });
-            callback(oAuth2Client);
-        });
-    });
+    try {
+        await auth.authorize();
+        console.log('Successfully connected!');
+        return auth;
+    } catch (error) {
+        console.error('Error connecting to Google Sheets API:', error);
+    }
 }
 
 function getSubstancesAndTypes(auth) {
@@ -141,12 +106,18 @@ function getSubstancesAndTypes(auth) {
             () => console.log('Wrote substanceTypeCollection.json')
         );
 
-        // Commenting out this section as it's no longer needed
-        // fs.writeFile(
-        //   './substanceList.json',
-        //   JSON.stringify(substanceList),
-        //   { encoding: 'utf8' },
-        //   () => console.log('Wrote substanceList.json')
-        // );
+        fs.writeFile(
+            './substanceList.json',
+            JSON.stringify(substanceList),
+            { encoding: 'utf8' },
+            () => console.log('Wrote substanceList.json')
+        );
     });
 }
+
+(async () => {
+    const auth = await authenticate();
+    if (auth) {
+        getSubstancesAndTypes(auth);
+    }
+})();
